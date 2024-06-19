477328b8d7f1b0244ad4c91a861309560f11944d
Devansh Vishwakarma <devwish0207@gmail.com>
2024-06-19 22:58:58 +0530

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import EventRegistrationForm from './components/EventRegistrationForm';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
